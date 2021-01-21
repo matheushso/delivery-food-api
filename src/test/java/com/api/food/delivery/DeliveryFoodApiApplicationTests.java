f9f1c7cdc1e1e13a199898f93b57ad45651173c3
Matheus Oliveira <matheushso99@hotmail.com>
2021-01-20 23:23:37 -0300

    first commit

---
+++ src/test/java/com/api/food/delivery/DeliveryFoodApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.food.delivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryFoodApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
